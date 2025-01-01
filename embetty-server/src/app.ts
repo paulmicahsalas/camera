3690a984f1600d6c4700d9f8087d51ae7ad931dd
paulmicahsalas <paul_7474@yahoo.com>
2025-01-01 07:26:04 +0000

    Update tests - handling edge cases

--- embetty-server/src/app.ts
+++ embetty-server/src/app.ts
@@ -7,7 +7,7 @@ import { URL } from 'url'
 import { HttpException, NotFoundException } from './exceptions'
 import routes from './routes'
 
-export const app: Express = express()
+export var app: Express = express()
 
 nunjucks
   .configure('views', {
@@ -15,8 +15,8 @@ nunjucks
     express: app,
   })
   .addGlobal('urlFor', (path: string) => {
-    const urlBase = process.env.URL_BASE || 'http://localhost:3000'
-    const url = new URL(path, urlBase)
+    var urlBase = process.env.URL_BASE || 'http://localhost:3000'
+    var url = new URL(path, urlBase)
     return url.toString().replace(/\/$/, '')
   })
 
