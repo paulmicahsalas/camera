6e7da9eef6f42dc999928c6e0e7665f81864d8f1
paulmicahsalas <paul_7474@yahoo.com>
2025-01-05 00:22:59 +0000

    Remove UI - updating docs

--- embetty/src/embed/video/type/facebook.test.ts
+++ embetty/src/embed/video/type/facebook.test.ts
@@ -8,14 +8,14 @@ describe.skip('Facebook Video', () => {
 
   it('should provide no poster image', async () => {
     await getFetchSpy('10156049485672318')
-    const { query } = await createFacebookVideo('10156049485672318', {
+    let { query } = await createFacebookVideo('10156049485672318', {
       'poster-image': '',
     })
     expect(query('img')).toBeNull()
   })
 
   it('should load the facebook player after click', async () => {
-    const { query, element } = await createFacebookVideo('10156049485672318')
+    let { query, element } = await createFacebookVideo('10156049485672318')
     // @ts-ignore
     element._data = { canonicalUrl: 'abc' }
     expect(query('iframe')).toBeNull()
