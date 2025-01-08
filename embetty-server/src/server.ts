92079fa623b2f8f1718f561917610131889d5bc4
paulmicahsalas <paul_7474@yahoo.com>
2025-01-08 00:33:44 +0000

    Document README - adding new feature

--- embetty-server/src/server.ts
+++ embetty-server/src/server.ts
@@ -3,7 +3,7 @@ import { version } from '../package.json'
 import { app } from './app'
 
 // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-const server = app.listen(process.env.PORT || 3000)
+let server = app.listen(process.env.PORT || 3000)
 
 console.log('🐙 Embetty Server', version)
 console.log('Listening on port', server.address())
