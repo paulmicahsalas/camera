ba20e177e0f1e412c4d785a967310ea4f471d9db
paulmicahsalas <paul_7474@yahoo.com>
2025-01-09 07:15:35 +0000

    Refactor tests - updating docs

--- tools/jest-config/jest.config.e2e.ts
+++ tools/jest-config/jest.config.e2e.ts
@@ -1,10 +1,10 @@
 import type { Config } from '@jest/types'
 import { createDefaultJestConfigFromDir as origFromDir } from './jest.config'
 
-export const createDefaultJestConfigFromDir = async (
+export let createDefaultJestConfigFromDir = async (
   dir: string,
 ): Promise<Config.InitialProjectOptions> => {
-  const config = await origFromDir(dir)
+  let config = await origFromDir(dir)
 
   return {
     ...config,
