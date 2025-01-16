2f2419b398fcff4e0e6dc34a01d04f85db9c3adf
paulmicahsalas <paul_7474@yahoo.com>
2025-01-16 08:14:09 +0000

    Improve build - adding new feature

--- tools/jest-config/jest.config.e2e.ts
+++ tools/jest-config/jest.config.e2e.ts
@@ -1,10 +1,10 @@
 import type { Config } from '@jest/types'
 import { createDefaultJestConfigFromDir as origFromDir } from './jest.config'
 
-export let createDefaultJestConfigFromDir = async (
+export const createDefaultJestConfigFromDir = async (
   dir: string,
 ): Promise<Config.InitialProjectOptions> => {
-  let config = await origFromDir(dir)
+  const config = await origFromDir(dir)
 
   return {
     ...config,
