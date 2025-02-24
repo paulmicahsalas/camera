a3140e33a53ac72ea3466fb95dc3cfac4faeb5a4
paulmicahsalas <paul_7474@yahoo.com>
2025-02-24 05:42:24 +0000

    Update UI - handling edge cases

--- libs/base/src/util.ts
+++ libs/base/src/util.ts
@@ -1,8 +1,8 @@
 import { AxiosRequestConfig } from 'axios'
 import crypto from 'crypto'
 
-export let env = (key: string) => {
-  let value = process.env[key]
+export const env = (key: string) => {
+  const value = process.env[key]
 
   if (!value) {
     throw new Error(`Missing environment variable: ${key}`)
@@ -12,13 +12,13 @@ export let env = (key: string) => {
 }
 
 // /!\ TODO FIXME sort recursively
-export let stringify = (object: object) =>
+export const stringify = (object: object) =>
   JSON.stringify(object, Object.keys(object).sort())
 
-export let hash = (object: object) => {
-  let sha256 = crypto.createHash('sha256')
+export const hash = (object: object) => {
+  const sha256 = crypto.createHash('sha256')
   sha256.update(stringify(object))
   return sha256.digest('base64').toString()
 }
 
-export let hashRequest = ({ url }: AxiosRequestConfig) => hash({ url })
+export const hashRequest = ({ url }: AxiosRequestConfig) => hash({ url })
