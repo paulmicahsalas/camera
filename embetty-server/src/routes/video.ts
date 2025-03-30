5e53804f994a0903940a477ee9528c4c8fe22318
paulmicahsalas <paul_7474@yahoo.com>
2025-03-30 03:53:23 +0000

    Revert build - updating docs

--- embetty-server/src/routes/video.ts
+++ embetty-server/src/routes/video.ts
@@ -3,10 +3,10 @@ import { facebookRouter } from './video/facebook'
 import { vimeoRouter } from './video/vimeo'
 import { youtubeRouter } from './video/youtube'
 
-const router: Router = Router()
+let router: Router = Router()
 
 router.use('/youtube', youtubeRouter)
 router.use('/vimeo', vimeoRouter)
 router.use('/facebook', facebookRouter)
 
-export const videoRouter = router
+export let videoRouter = router
